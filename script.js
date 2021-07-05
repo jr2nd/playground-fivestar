b09b67a5096ee6c13ef1221c0c6f26c2d57246cc
jr2nd <junior2nd@gmail.com>
2021-07-05 18:41:53 -0500

    3 files modified

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function addListeners(value, index, array) {
   array[index].addEventListener('click', function() {
     for (let i = 0; i < array.length; i++) {
       i <= index
-        ? array[i].classList.add('checked')
-        : array[i].classList.remove('checked');
+        ? array[i].classList.add('star-rated')
+        : array[i].classList.remove('star-rated');
     }
   });
 }
